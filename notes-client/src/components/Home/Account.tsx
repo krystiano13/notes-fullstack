afaf95a83c0c2658f0d21a00dfda0eb4440f43ff
krystiano13 <crystianotv@gmail.com>
2023-08-28 11:23:35 +0200

    add styles for desktop version of home

--- notes-client/src/components/Home/Account.tsx
+++ notes-client/src/components/Home/Account.tsx
@@ -2,18 +2,38 @@ import waves from "../../assets/images/layered-waves-haikei.svg";
 
 export const Account = () => {
   return (
-    <section class={"Home__account position-absolute"}>
-      <h1>Notes App</h1>
-      <h2>Register</h2>
-      <form class={"Home__account__form"}>
-        <input name="username" placeholder={"username"} type={"text"} />
-        <input name={"password"} placeholder={"password"} type="password" />
+    <section
+      class={
+        "Home__account d-flex flex-column justify-content-end position-absolute"
+      }
+    >
+      <form
+        class={"Home__account__form position-absolute p-5 d-flex flex-column"}
+      >
+        <h1 class="font-head fw-bold"><span>Notes</span> App</h1>
+        <h2 class="font-head">Register</h2>
         <input
+          class="mt-3"
+          name="username"
+          placeholder={"username"}
+          type={"text"}
+        />
+        <input
+          class="mt-3"
+          name={"password"}
+          placeholder={"password"}
+          type="password"
+        />
+        <input
+          class="mt-3"
           name={"password2"}
           placeholder={"repeat password"}
           type="password"
         />
-        <button type={"submit"}>Register</button>
+        <button class="mt-3 fw-bold" type={"submit"}>
+          Register
+        </button>
+        <p class="switch mt-3">Have got an account already ?</p>
       </form>
       <img class="Home__account__waves" src={waves} alt="waves" />
     </section>
