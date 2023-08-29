795fe0fe175e8dc2973866437ab6504c3806de5c
krystiano13 <crystianotv@gmail.com>
2023-08-29 11:42:12 +0200

    add login function on client side

---
+++ notes-client/src/functions/login.ts
@@ -0,0 +1,27 @@
+const login = async (
+  username: string,
+  password: string,
+  onSuccess: () => void,
+  onFailure: () => void
+) => {
+  const formData = new FormData();
+  formData.append("username", username);
+  formData.append("password", password);
+
+  await fetch("http://127.0.0.1:8000/api/login", {
+    method: "post",
+    body: formData,
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.msg) {
+        onSuccess();
+        return;
+      } else {
+        onFailure();
+        return;
+      }
+    });
+};
+
+export { login };
